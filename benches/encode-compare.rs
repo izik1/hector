157af03035849d531d934c64cbf19bca1256c0c2
Skyler Rain Ross <orangesnowfox@gmail.com>
2024-01-06 17:35:23 -0800

    chore: update benchmarks and deps

--- benches/encode-compare.rs
+++ benches/encode-compare.rs
@@ -1,28 +1,81 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
 use rand::RngCore;
 
 // todo: add benchmarks for other encoding functions.
 fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("to-string");
+    const SIZES: [usize; 6] = [2, 8, 32, 128, 512, 2048];
+    {
+        let mut group = c.benchmark_group("to-string");
 
-    for size in [2, 8, 32, 128, 512, 2048] {
+        for size in SIZES {
+            let mut input = vec![0; size];
+
+            rand::thread_rng().fill_bytes(&mut input);
+
+            group
+                .throughput(Throughput::Bytes(input.len() as u64))
+                .bench_with_input(BenchmarkId::new("hex", input.len()), &input, |b, input| {
+                    b.iter(|| hex::encode(&input))
+                })
+                .bench_with_input(
+                    BenchmarkId::new("faster-hex", input.len()),
+                    &input,
+                    |b, input| b.iter(|| faster_hex::hex_string(&input)),
+                )
+                .bench_with_input(
+                    BenchmarkId::new("hector", input.len()),
+                    &input,
+                    |b, input| b.iter(|| hector::encode(&input)),
+                );
+        }
+    }
+
+    let mut group = c.benchmark_group("to-slice");
+
+    for size in SIZES {
         let mut input = vec![0; size];
 
         rand::thread_rng().fill_bytes(&mut input);
 
         group
+            .throughput(Throughput::Bytes(input.len() as u64))
             .bench_with_input(BenchmarkId::new("hex", input.len()), &input, |b, input| {
-                b.iter(|| hex::encode(&input))
+                b.iter_batched(
+                    || (input, vec![0; size * 2]),
+                    |(input, mut output)| {
+                        hex::encode_to_slice(&input, &mut output).unwrap();
+                        unsafe { String::from_utf8_unchecked(output) }
+                    },
+                    BatchSize::SmallInput,
+                )
             })
             .bench_with_input(
                 BenchmarkId::new("faster-hex", input.len()),
                 &input,
-                |b, input| b.iter(|| faster_hex::hex_string(&input)),
+                |b, input| {
+                    b.iter_batched(
+                        || (input, vec![0; size * 2]),
+                        |(input, mut output)| {
+                            faster_hex::hex_encode(&input, &mut output).unwrap();
+                            unsafe { String::from_utf8_unchecked(output) }
+                        },
+                        BatchSize::SmallInput,
+                    )
+                },
             )
             .bench_with_input(
                 BenchmarkId::new("hector", input.len()),
                 &input,
-                |b, input| b.iter(|| hector::encode(&input)),
+                |b, input| {
+                    b.iter_batched(
+                        || (input, vec![0; size * 2]),
+                        |(input, mut output)| {
+                            hector::encode_to_slice(&input, &mut output).unwrap();
+                            unsafe { String::from_utf8_unchecked(output) }
+                        },
+                        BatchSize::SmallInput,
+                    )
+                },
             );
     }
 }
